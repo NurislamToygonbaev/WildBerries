813fb5f97afb93325003fb57e532b20a4f97cb31
Nur0606 <seidekmatov@gmail.com>
2024-01-10 17:28:50 +0600

    announcement added daoimpl

--- src/dao/daoImpl/AnnouncementDaoImpl.java
+++ src/dao/daoImpl/AnnouncementDaoImpl.java
@@ -2,7 +2,11 @@ package dao.daoImpl;
 
 import dao.AnnouncementDao;
 import database.DataBase;
+import exception.MyException;
 import models.Announcement;
+import models.Users;
+
+import java.util.Optional;
 
 public class AnnouncementDaoImpl implements AnnouncementDao {
     private final DataBase dataBase;
@@ -13,21 +17,59 @@ public class AnnouncementDaoImpl implements AnnouncementDao {
 
     @Override
     public boolean save(Announcement announcement) {
-        return false;
+        for (Users users:dataBase.getAll()){
+           return users.getAnnouncements().add(announcement);
+        }
+        throw new MyException("error!");
     }
 
     @Override
-    public Announcement getById(Long id) {
-        return null;
+    public Announcement getById(Long id){
+        for (Users users:dataBase.getAll()){
+            for (Announcement announcement:users.getAnnouncements()){
+                if (announcement.getId().equals(id)){
+                    return announcement;
+                }
+            }
+
+        }
+        throw new MyException("announcement woth id: "+id+ " not found!");
     }
 
     @Override
     public boolean update(Announcement announcement) {
-        return false;
+        for (Users users : dataBase.getAll()) {
+            for (Announcement usersAnnouncement : users.getAnnouncements()) {
+                usersAnnouncement.setName(announcement.getName());
+                usersAnnouncement.setDescription(announcement.getDescription());
+                usersAnnouncement.setPrice(announcement.getPrice());
+                return true;
+            }
+        }
+        throw new MyException("error");
     }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        for (Users users:dataBase.getAll()){
+            for (Announcement announcement:users.getAnnouncements()){
+                if (announcement.getId().equals(id)){
+                   return users.getAnnouncements().remove(announcement);
+                }
+            }
+        }
+        throw new MyException("announcement woth id: "+id+ " not found!");
+    }
+
+    @Override
+    public Announcement findByAnonsimentName(String name) {
+        for (Users users:dataBase.getAll()){
+            for (Announcement announcement:users.getAnnouncements()){
+                if (announcement.getName().equals(name)){
+                    return announcement;
+                }
+            }
+        }
+        throw new MyException("announcement with name: "+name+ " not found!");
     }
 }
